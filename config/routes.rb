229ea431e6a7710fa1f628b1decdf39424309c4b
sousukegit <greenneosoul.sousuke@gmail.com>
2024-02-13 07:39:58 +0900

    APIテストを実施

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+   # 追加
+  namespace :api do
+    namespace :v1 do
+      # api test action
+      resources :hello, only:[:index]
+    end
+  end
 end
