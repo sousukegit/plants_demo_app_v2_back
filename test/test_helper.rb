60daa5dba3c3f5e20dbb30a9268bee7050bda342
sousukegit <greenneosoul.sousuke@gmail.com>
2024-02-19 07:31:52 +0900

    Uerテーブルバリデーションテスト完了

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,16 +12,19 @@ class ActiveSupport::TestCase
   # プロセスが分岐した直後に呼び出し
   parallelize_setup do |worker|
     load "#{Rails.root}/db/seeds.rb"
-  end
- 
+  end 
 
   # 並列テストの有効化
   #workers プロセス数を渡す（2以上なら実行）
   # :number_of_processors マシンのコア数（docker） 240218では12コアだった
   parallelize(workers: :number_of_processors)
 
+  def active_user
+    User.find_by(activated: true)
+  end
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
