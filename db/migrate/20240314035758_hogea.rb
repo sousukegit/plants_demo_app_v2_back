fddea58d82e32b2beb598b62890afb9d18923853
sousukegit <greenneosoul.sousuke@gmail.com>
2024-04-21 10:47:28 +0900

    デプロイ時RDSにdb作成seeds作成まで完了、app_rootのエラー対応中

---
+++ db/migrate/20240314035758_hogea.rb
@@ -0,0 +1,5 @@
+class Hogea < ActiveRecord::Migration[7.0]
+    def change
+    end
+end
+  
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,18 @@ rm -f /app/tmp/pids/server.pid
 # WARNING:createとseedはfargateの初回のみ実行
 
 #20240421 PSG→MYSQLに変更したのでcastなどのmigrateファイルが一部通らない
-#bundle exec rails db:create
-#bundle exec rails db:migrate
-#undle exec rails db:seed
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
 
 #megrateせずsceamaの読み込みとSeedだけ行う
 #bundle exec rails db:setup
 #ActiveRecord::NoEnvironmentInSchemaError: がでる
 
 #命令通り、Tasks: TOP => db:setup => db:schema:load => db:check_protected_environmentsを実行 
-bundle exec rails db:setup
-bundle exec rails db:schema:load
-bundle exec rails db:check_protected_environments
+# bundle exec rails db:setup
+# bundle exec rails db:schema:load
+# bundle exec rails db:check_protected_environments
 #起動
 bundle exec pumactl start
 echo "Puma started"
