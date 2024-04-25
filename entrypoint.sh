1aa0d2f29b7889482d5052a07794ebb78656fb1d
sousukegit <greenneosoul.sousuke@gmail.com>
2024-04-26 07:27:15 +0900

    server_nameのサーバー名をサブドメインに変更

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,15 @@
 set -e
 # サーバープロセスが存在しないことを確認してから削除
 rm -f /app/tmp/pids/server.pid
-# WARNING:createとseedはfargateの初回のみ実行
+
 
 #20240421 PSG→MYSQLに変更したのでcastなどのmigrateファイルが一部通らない
-bundle exec rails db:create
-bundle exec rails db:migrate
-bundle exec rails db:seed
+#migrationファイルを整理して対応
+# WARNING:createとseedはfargateの初回のみ実行
+# bundle exec rails db:create
+# bundle exec rails db:migrate
+# bundle exec rails db:seed
 
-#megrateせずsceamaの読み込みとSeedだけ行う
-#bundle exec rails db:setup
-#ActiveRecord::NoEnvironmentInSchemaError: がでる
-
-#命令通り、Tasks: TOP => db:setup => db:schema:load => db:check_protected_environmentsを実行 
-# bundle exec rails db:setup
-# bundle exec rails db:schema:load
-# bundle exec rails db:check_protected_environments
 #起動
 bundle exec pumactl start
 echo "Puma started"
